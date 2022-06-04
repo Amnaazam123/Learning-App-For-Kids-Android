bca9372cee4c77f78128b5b240662ee6eeca4649
Amnaazam123 <67511883+Amnaazam123@users.noreply.github.com>
2022-06-05 04:50:33 +0500

    2 button activity added

---
+++ app/src/main/java/com/example/learning_app/AlphabetsOrNames.java
@@ -0,0 +1,36 @@
+package com.example.learning_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class AlphabetsOrNames extends AppCompatActivity {
+
+    Button learnabc;
+    Button learnnames;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_alphabets_or_names);
+        learnabc=findViewById(R.id.learnabc);
+        learnnames=findViewById(R.id.learnnames);
+        learnabc.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(AlphabetsOrNames.this, Alphabets.class));
+
+            }
+        });
+        learnnames.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(AlphabetsOrNames.this, learnMoreNames.class));
+
+            }
+        });
+        
+    }
+}
--- app/src/main/java/com/example/learning_app/MainActivity.java
+++ app/src/main/java/com/example/learning_app/MainActivity.java
@@ -23,7 +23,7 @@ public class MainActivity extends AppCompatActivity {
         learn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                startActivity(new Intent(MainActivity.this, Alphabets.class));
+                startActivity(new Intent(MainActivity.this, AlphabetsOrNames.class));
 
             }
         });
