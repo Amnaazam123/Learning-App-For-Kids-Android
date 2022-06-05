ee2fbf81a21a59d56683360471c653487b0ad0a3
Amnaazam123 <67511883+Amnaazam123@users.noreply.github.com>
2022-06-05 19:01:14 +0500

    listAdapter added

--- app/src/main/java/com/example/learning_app/ListAdapter.java
+++ app/src/main/java/com/example/learning_app/ListAdapter.java
@@ -1,4 +1,41 @@
 package com.example.learning_app;
 
-public class ListAdapter {
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import java.util.ArrayList;
+
+public class ListAdapter extends ArrayAdapter<myClass> {
+    public ListAdapter(Context context, ArrayList<myClass> myClassArrayList){
+        super(context,R.layout.list_item,myClassArrayList);
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        myClass myclass=getItem(position);
+        if(convertView==null){
+            convertView= LayoutInflater.from(getContext()).inflate(R.layout.list_item,parent,false);
+
+        }
+        ImageView imageview = convertView.findViewById(R.id.imageheading);
+        TextView fruittv=convertView.findViewById(R.id.fruitnames);
+        TextView subtv=convertView.findViewById(R.id.submsg);
+        imageview.setImageResource(myclass.imageid);
+        fruittv.setText(myclass.heading);
+        subtv.setText(myclass.subheading);
+
+
+
+
+        return super.getView(position, convertView, parent);
+    }
 }
